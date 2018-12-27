f6fbd924ac5b155a46f19ea46becb33fba6efb9d
Damian F <damian.fikier@wp.pl>
2018-12-27 22:18:28 +0100

    still working on ef core

--- GenTreeApp.Persistence/TreeDbContext.cs
+++ GenTreeApp.Persistence/TreeDbContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using GenTreeApp.Domain.Entities;
+using GenTreeApp.Persistence.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace GenTreeApp.Persistence
@@ -9,14 +10,28 @@ namespace GenTreeApp.Persistence
     public class TreeDbContext : DbContext
     {
 
-        public virtual DbSet<Tree> Trees { get; set; }
-        public virtual DbSet<User> Users { get; set; }
-        public virtual DbSet<Relation> Relations { get; set; }
-        public virtual DbSet<Details> Details { get; set; }
-        public virtual DbSet<Event> Events { get; set; }
-        public virtual DbSet<Media> Media { get; set; }
-        public virtual DbSet<Comment> Comments { get; set; }
-        public virtual DbSet<Person> Persons { get; set; }
+        public DbSet<Tree> Trees { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Relation> Relations { get; set; }
+        public DbSet<Details> Details { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Media> Media { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Person> Persons { get; set; }
 
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+         
+
+            modelBuilder.ApplyConfiguration(new PersonConfiguration());
+            modelBuilder.ApplyConfiguration(new TreeConfiguration());
+            modelBuilder.ApplyConfiguration(new RelationConfiguration());
+            modelBuilder.ApplyConfiguration(new EventConfiguration());
+            modelBuilder.ApplyConfiguration(new MediaConfiguration());
+            modelBuilder.ApplyConfiguration(new DetailsConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
