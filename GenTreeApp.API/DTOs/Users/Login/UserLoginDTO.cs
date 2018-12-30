332ca7c5df152e74258aec4706b8a98ee9af4c55
Damian F <damian.fikier@wp.pl>
2018-12-30 12:12:59 +0100

    working database

---
+++ GenTreeApp.API/DTOs/Users/Login/UserLoginDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenTreeApp.API.DTOs.Users.Login
+{
+    public class UserLoginDto
+    {
+
+    }
+}
